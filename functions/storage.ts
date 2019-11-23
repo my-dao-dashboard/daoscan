6af360d2eb4f9fa5041ec24b25fbcd2857b0e01d
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-11-23 04:25:41 +0400

    Save org creation event

--- functions/storage.ts
+++ functions/storage.ts
@@ -1,3 +1,48 @@
-export async function saveOrganisationEvent(event: any, context: any) {
-  console.log('event', event)
+import { ORGANISATION_EVENT, OrganisationEvent } from "../lib/organisation-events";
+import AWS from "aws-sdk";
+import { UnreachableCaseError } from "../lib/unreachable-case-error";
+
+interface SqsEvent {
+  Records: { body: string }[];
+}
+
+const ORGANISATIONS_TABLE = String(process.env.ORGANISATIONS_TABLE);
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+export async function saveOrganisationEvent(event: SqsEvent, context: any) {
+  const queries = event.Records.map(r => {
+    const event = JSON.parse(r.body) as OrganisationEvent;
+    switch (event.kind) {
+      case ORGANISATION_EVENT.CREATED:
+        return {
+          TableName: ORGANISATIONS_TABLE,
+          Item: {
+            address: event.address,
+            name: event.name,
+            platform: event.platform,
+            txid: event.txid,
+            timestamp: event.timestamp,
+            blockNumber: event.blockNumber,
+          }
+        };
+      case ORGANISATION_EVENT.APP_INSTALLED:
+        return;
+      default:
+        throw new UnreachableCaseError(event);
+    }
+  });
+
+  const writings = queries.map(q => {
+    return new Promise((resolve, reject) => {
+      if (q) {
+        dynamo.put(q, (error, ok) => {
+          error ? reject(error) : resolve(ok)
+        })
+      } else {
+        resolve()
+      }
+    })
+  })
+
+  await Promise.all(writings)
 }
