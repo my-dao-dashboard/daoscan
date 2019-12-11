53b593441e7b99afe179937413561980427961f2
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-12-12 02:25:34 +0400

    Rename serverless stuff

--- functions/scraping.ts
+++ functions/scraping.ts
@@ -4,7 +4,13 @@ import { EthereumService } from "../lib/ethereum.service";
 import AWS from "aws-sdk";
 import { DynamoService } from "../lib/storage/dynamo.service";
 import { TOKEN_ABI, TOKEN_CONTROLLER_ABI } from "../lib/scraping/aragon.constants";
-import { AddParticipantEvent, ORGANISATION_EVENT, ORGANISATION_PLATFORM } from "../lib/organisation-events";
+import {
+  AddParticipantEvent, AppInstalledEvent,
+  ORGANISATION_EVENT,
+  ORGANISATION_PLATFORM, OrganisationCreatedEvent,
+  OrganisationEvent, ShareTransferEvent
+} from "../lib/organisation-events";
+import {UnreachableCaseError} from "../lib/unreachable-case-error";
 
 const BLOCKS_SQS_URL = String(process.env.BLOCKS_SQS_URL);
 const SCRAPING_SQS_URL = String(process.env.SCRAPING_SQS_URL);
@@ -13,6 +19,8 @@ const INFURA_PROJECT_ID = String(process.env.INFURA_PROJECT_ID);
 
 const APPLICATIONS_TABLE = String(process.env.APPLICATIONS_TABLE);
 const BLOCKS_TABLE = String(process.env.BLOCKS_TABLE);
+const PARTICIPANTS_TABLE = String(process.env.PARTICIPANTS_TABLE);
+const ORGANISATIONS_TABLE = String(process.env.ORGANISATIONS_TABLE);
 
 const ethereum = new EthereumService(INFURA_PROJECT_ID);
 const dynamo = new DynamoService();
@@ -154,4 +162,93 @@ export async function readExtendedBlock(event: any) {
   const block = await ethereum.extendedBlock(id)
   const events = await scraping.fromBlock(id);
   return ok({block, events})
+}
+
+interface SqsEvent {
+  Records: { body: string }[];
+}
+
+async function handleCreateOrganisation(event: OrganisationCreatedEvent): Promise<void> {
+  await dynamo.put({
+    TableName: ORGANISATIONS_TABLE,
+    Item: {
+      address: event.address,
+      name: event.name,
+      platform: event.platform,
+      txid: event.txid,
+      timestamp: event.timestamp,
+      blockNumber: event.blockNumber
+    }
+  });
+}
+
+async function handleInstallApplication(event: AppInstalledEvent): Promise<void> {
+  await dynamo.put({
+    TableName: APPLICATIONS_TABLE,
+    Item: {
+      organisationAddress: event.organisationAddress,
+      appId: event.appId,
+      proxyAddress: event.proxyAddress,
+      platform: event.platform,
+      txid: event.txid,
+      blockNumber: event.blockNumber,
+      timestamp: event.timestamp
+    }
+  });
+}
+
+async function handleAddParticipant(event: AddParticipantEvent) {
+  console.log("trying to put", {
+    TableName: PARTICIPANTS_TABLE,
+    Item: {
+      organisationAddress: event.organisationAddress,
+      participantAddress: event.participant,
+      updatedAt: new Date().valueOf()
+    }
+  });
+  await dynamo.put({
+    TableName: PARTICIPANTS_TABLE,
+    Item: {
+      organisationAddress: event.organisationAddress,
+      participantAddress: event.participant,
+      updatedAt: new Date().valueOf()
+    }
+  });
+}
+
+async function putParticipant(event: ShareTransferEvent, account: string) {
+  if (account !== '0x0000000000000000000000000000000000000000') {
+    await dynamo.put({
+      TableName: PARTICIPANTS_TABLE,
+      Item: {
+        organisationAddress: event.organisationAddress,
+        participantAddress: account
+      }
+    })
+  }
+}
+
+async function handleTransferShare(event: ShareTransferEvent) {
+  await putParticipant(event, event.from);
+  await putParticipant(event, event.to);
+}
+
+export async function saveOrganisationEvent(event: SqsEvent, context: any): Promise<void> {
+  const loop = event.Records.map(async r => {
+    const event = JSON.parse(r.body) as OrganisationEvent;
+    switch (event.kind) {
+      case ORGANISATION_EVENT.APP_INSTALLED:
+        return handleInstallApplication(event);
+      case ORGANISATION_EVENT.CREATED:
+        return handleCreateOrganisation(event);
+      case ORGANISATION_EVENT.ADD_PARTICIPANT:
+        return handleAddParticipant(event);
+      case ORGANISATION_EVENT.TRANSFER_SHARE:
+        return handleTransferShare(event);
+      default:
+        throw new UnreachableCaseError(event);
+    }
+  });
+
+  await Promise.all(loop);
 }
