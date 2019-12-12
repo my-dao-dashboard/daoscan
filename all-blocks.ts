a7f2b557757eedbaa6de0c9087ef0aa467f13fa5
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-12-12 23:41:30 +0400

    code style

--- all-blocks.ts
+++ all-blocks.ts
@@ -1,26 +1,26 @@
-import axios from 'axios'
-import axiosRetry from 'axios-retry';
-import * as _ from 'lodash'
+import axios from "axios";
+import axiosRetry from "axios-retry";
+import * as _ from "lodash";
 
 axiosRetry(axios, { retries: 3, retryCondition: () => true });
-import * as fs from 'fs'
+import * as fs from "fs";
 
-const aa = JSON.parse(fs.readFileSync('./a.json').toString())
+const aa = JSON.parse(fs.readFileSync("./a.json").toString());
 
-const blocks = aa.items.Items.map((e: any) => e.blockNumber)
+const blocks = aa.items.Items.map((e: any) => e.blockNumber);
 
 async function main() {
   for (let i = 800; i < blocks.length; i = i + 10) {
     const promises = _.times(10).map(async j => {
       if (blocks[i + j]) {
-        return axios.post('https://d2revro29l.execute-api.us-east-1.amazonaws.com/dev/block', {
+        return axios.post("https://d2revro29l.execute-api.us-east-1.amazonaws.com/dev/block", {
           id: blocks[i + j]
-        })
+        });
       }
-    })
-    await Promise.all(promises)
-    console.log(`Done ${i} out of ${blocks.length}`)
+    });
+    await Promise.all(promises);
+    console.log(`Done ${i} out of ${blocks.length}`);
   }
 }
 
-main()
+main();
