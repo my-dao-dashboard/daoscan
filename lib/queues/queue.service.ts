74ef521bc6a12d49063c4eaa05a014247c12951f
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-12-12 23:41:02 +0400

    add all-participants.ts script

--- lib/queues/queue.service.ts
+++ lib/queues/queue.service.ts
@@ -1,4 +1,5 @@
 import { SQS } from "aws-sdk";
+import * as _ from "lodash";
 
 export class QueueService {
   private readonly sqs: SQS;
@@ -9,23 +10,30 @@ export class QueueService {
 
   async sendBatch(queueUrl: string, payloads: any[]): Promise<void> {
     if (payloads.length) {
-      const entries = payloads.map<SQS.Types.SendMessageBatchRequestEntry>((p, i) => {
-        return {
-          Id: i.toString(),
-          MessageBody: JSON.stringify(p)
-        };
+      const batches = _.chunk(payloads, 10).map(async p => {
+        await this.sendBatchInternal(queueUrl, p);
       });
-      const message: SQS.Types.SendMessageBatchRequest = {
-        QueueUrl: queueUrl,
-        Entries: entries
-      };
-      console.log(`Posting message to queue ${queueUrl}`, message);
-      return new Promise((resolve, reject) => {
-        this.sqs.sendMessageBatch(message, error => {
-          error ? reject(error) : resolve();
-        });
-      });
+      await Promise.all(batches);
     }
+  }
+
+  private async sendBatchInternal(queueUrl: string, payloads: any[]): Promise<void> {
+    const entries = payloads.map<SQS.Types.SendMessageBatchRequestEntry>((p, i) => {
+      return {
+        Id: i.toString(),
+        MessageBody: JSON.stringify(p)
+      };
+    });
+    const message: SQS.Types.SendMessageBatchRequest = {
+      QueueUrl: queueUrl,
+      Entries: entries
+    };
+    console.log(`Posting message to queue ${queueUrl}`, message);
+    return new Promise((resolve, reject) => {
+      this.sqs.sendMessageBatch(message, error => {
+        error ? reject(error) : resolve();
+      });
+    });
   }
 
   send(queueUrl: string, payload: any): Promise<SQS.SendMessageResult> {
