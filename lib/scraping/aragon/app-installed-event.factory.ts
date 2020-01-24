496d36395caf3f9e789984cdd56bd9dbc17654d5
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-01-24 16:48:15 +0400

    Handle app installations

---
+++ lib/scraping/aragon/app-installed-event.factory.ts
@@ -0,0 +1,111 @@
+import { Inject, Service } from "typedi";
+import { EthereumService } from "../../services/ethereum.service";
+import { PLATFORM } from "../../domain/platform";
+import { Block } from "../block";
+import { ConnectionFactory } from "../../storage/connection.factory";
+import { SCRAPING_EVENT_KIND } from "../events/scraping-event.interface";
+import { AppInstalledEvent } from "../events/scraping-event";
+import { logEvents } from "./events-from-logs";
+import { BlockchainEvent } from "../blockchain-event";
+import { APP_ID } from "../../storage/app-id";
+import { TOKEN_CONTROLLER_ABI } from "./token-controller.abi";
+
+export interface NewAppProxyParams {
+  proxy: string;
+  isUpgradeable: boolean;
+  appId: string;
+}
+
+// New application installed
+// NewAppProxy(address proxy, bool isUpgradeable, bytes32 appId)
+export const NEW_APP_PROXY_EVENT: BlockchainEvent<NewAppProxyParams> = {
+  signature: "0xd880e726dced8808d727f02dd0e6fdd3a945b24bfee77e13367bcbe61ddbaf47",
+  abi: [
+    {
+      type: "address",
+      name: "proxy"
+    },
+    {
+      type: "bool",
+      name: "isUpgradeable"
+    },
+    {
+      type: "bytes32",
+      name: "appId"
+    }
+  ]
+};
+
+@Service(AppInstalledEventFactory.name)
+export class AppInstalledEventFactory {
+  constructor(
+    @Inject(EthereumService.name) private readonly ethereum: EthereumService,
+    @Inject(ConnectionFactory.name) private readonly connectionFactory: ConnectionFactory
+  ) {}
+
+  async kernelAddress(proxy: string): Promise<string> {
+    const data = this.ethereum.codec.encodeFunctionCall(
+      {
+        name: "kernel",
+        type: "function",
+        inputs: []
+      },
+      []
+    );
+    const result = await this.ethereum.call({
+      to: proxy,
+      data
+    });
+    return this.ethereum.codec.decodeParameter("address", result);
+  }
+
+  async fromEvents(block: Block): Promise<AppInstalledEvent[]> {
+    const extendedBlock = await block.extendedBlock();
+    const timestamp = await block.timestamp();
+    const nativeEvents = logEvents(this.ethereum.codec, extendedBlock, NEW_APP_PROXY_EVENT);
+    const appInstalledPromised = nativeEvents.map<Promise<AppInstalledEvent>>(async e => {
+      const organisationAddress = await this.kernelAddress(e.proxy);
+      return {
+        kind: SCRAPING_EVENT_KIND.APP_INSTALLED,
+        organisationAddress: organisationAddress.toLowerCase(),
+        appId: e.appId,
+        proxyAddress: e.proxy,
+        platform: PLATFORM.ARAGON,
+        blockNumber: e.blockNumber,
+        blockHash: block.hash,
+        timestamp: timestamp,
+        txid: e.txid
+      };
+    });
+    return Promise.all(appInstalledPromised);
+  }
+
+  async sharesEvents(native: AppInstalledEvent[]): Promise<AppInstalledEvent[]> {
+    const tokenControllerEvents = native.filter(e => {
+      return e.appId === APP_ID.ARAGON_TOKEN_CONTROLLER;
+    });
+    const promised = tokenControllerEvents.map<Promise<AppInstalledEvent>>(async e => {
+      const tokenControllerAddress = e.proxyAddress;
+      const tokenController = this.ethereum.contract(TOKEN_CONTROLLER_ABI, tokenControllerAddress);
+      const tokenAddress = await tokenController.methods.token().call();
+      return {
+        kind: SCRAPING_EVENT_KIND.APP_INSTALLED,
+        platform: PLATFORM.ARAGON,
+        organisationAddress: e.organisationAddress,
+        appId: APP_ID.SHARE,
+        proxyAddress: tokenAddress,
+        txid: e.txid,
+        blockHash: e.blockHash,
+        blockNumber: e.blockNumber,
+        timestamp: e.timestamp
+      };
+    });
+    return Promise.all(promised);
+  }
+
+  async fromBlock(block: Block): Promise<AppInstalledEvent[]> {
+    const nativeEvents = await this.fromEvents(block);
+    const sharesEvent = await this.sharesEvents(nativeEvents);
+    return nativeEvents.concat(sharesEvent);
+  }
+}
--- lib/scraping/aragon/aragon-event.factory.ts
+++ lib/scraping/aragon/aragon-event.factory.ts
@@ -2,16 +2,20 @@ import { Inject, Service } from "typedi";
 import { Block } from "../block";
 import { ScrapingEvent } from "../events/scraping-event";
 import { OrganisationCreatedEventFactory } from "./organisation-created-event.factory";
+import { AppInstalledEventFactory } from "./app-installed-event.factory";
 
 @Service(AragonEventFactory.name)
 export class AragonEventFactory {
   constructor(
     @Inject(OrganisationCreatedEventFactory.name)
-    private readonly organisationCreatedEventFactory: OrganisationCreatedEventFactory
+    private readonly organisationCreatedEventFactory: OrganisationCreatedEventFactory,
+    @Inject(AppInstalledEventFactory.name)
+    private readonly appInstalledEventFactory: AppInstalledEventFactory
   ) {}
 
   async fromBlock(block: Block): Promise<ScrapingEvent[]> {
-    const organisationCreatedEvents = await this.organisationCreatedEventFactory.fromBlock(block);
-    return organisationCreatedEvents;
+    const organisationCreatedEvents: ScrapingEvent[] = await this.organisationCreatedEventFactory.fromBlock(block);
+    const appInstalledEvents: ScrapingEvent[] = await this.appInstalledEventFactory.fromBlock(block);
+    return organisationCreatedEvents.concat(appInstalledEvents);
   }
 }
