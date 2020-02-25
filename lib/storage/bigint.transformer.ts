0bbdcf71e2affc70de7b22ef6c3eeef14a806b5e
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-02-25 20:24:50 +0400

    bigint transformer handles FindOperator argument

--- lib/storage/bigint.transformer.ts
+++ lib/storage/bigint.transformer.ts
@@ -1,6 +1,13 @@
-import { ValueTransformer } from "typeorm";
+import { FindOperator, FindOperatorType, ValueTransformer } from "typeorm";
 
 export const bigintTransformer: ValueTransformer = {
-  to: (entityValue: bigint | undefined) => entityValue?.toString(),
+  to: (entityValue: bigint | FindOperator<bigint> | undefined) => {
+    if (entityValue instanceof FindOperator) {
+      const type = (entityValue as any)._type as FindOperatorType; // Beware, private API
+      return new FindOperator(type, entityValue.value, entityValue.useParameter, entityValue.multipleParameters);
+    } else {
+      return entityValue?.toString();
+    }
+  },
   from: (databaseValue: string): bigint => BigInt(databaseValue)
 };
