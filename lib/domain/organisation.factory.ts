47698bbd32c491799670c7f16aa6e706c5c214e5
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-03-31 15:48:20 +0400

    Cleanup unnecessary code

--- lib/domain/organisation.factory.ts
+++ lib/domain/organisation.factory.ts
@@ -2,7 +2,6 @@ import { Inject, Service } from "typedi";
 import { Organisation } from "./organisation";
 import { EthereumService } from "../services/ethereum.service";
 import { OrganisationRepository } from "../storage/organisation.repository";
-import { ScrapingEventFactory } from "../scraping/events/scraping-event.factory";
 import { OrganisationService } from "./organisation.service";
 import { Organisation as OrganisationRow } from "../storage/organisation.row";
 
@@ -11,7 +10,6 @@ export class OrganisationFactory {
   constructor(
     @Inject(EthereumService.name) private readonly ethereum: EthereumService,
     @Inject(OrganisationRepository.name) private readonly organisationRepository: OrganisationRepository,
-    @Inject(ScrapingEventFactory.name) private readonly scrapingEventFactory: ScrapingEventFactory,
     @Inject(OrganisationService.name) private readonly organisationService: OrganisationService
   ) {}
 
--- lib/domain/vote.factory.ts
+++ lib/domain/vote.factory.ts
@@ -1,27 +1,17 @@
 import { Inject, Service } from "typedi";
 import { Vote as VoteRow } from "../storage/vote.row";
 import { Vote } from "./vote";
-import { HistoryRepository } from "../storage/history.repository";
-import { EventRepository } from "../storage/event.repository";
-import { MembershipRepository } from "../storage/membership.repository";
-import { OrganisationService } from "./organisation.service";
 import { OrganisationFactory } from "./organisation.factory";
 
 @Service(VoteFactory.name)
 export class VoteFactory {
-  constructor(
-    @Inject(HistoryRepository.name) private readonly historyRepository: HistoryRepository,
-    @Inject(EventRepository.name) private readonly eventRepository: EventRepository,
-    @Inject(OrganisationFactory.name) private readonly organisationFactory: OrganisationFactory
-  ) {}
+  constructor(@Inject(OrganisationFactory.name) private readonly organisationFactory: OrganisationFactory) {}
 
   async fromRow(row: VoteRow) {
-    const history = await this.historyRepository.forVote(row.id);
-    const event = await this.eventRepository.byIdOrFail(history.eventId);
     const organisation = await this.organisationFactory.byAddress(row.organisationAddress);
     const participant = await organisation?.participant(row.voter);
     return new Vote({
-      createdAt: event.timestamp,
+      createdAt: row.createdAt,
       voter: participant,
       decision: row.decision,
       organisationAddress: row.organisationAddress
